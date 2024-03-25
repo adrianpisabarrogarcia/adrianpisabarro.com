80bfb9bd7e2f724d89645bae3f524df1cef91ea3
Adrián Pisabarro García <adrian.pisabarro.garcia@gmail.com>
2024-03-25 22:21:17 +0100

    New header component

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'media',
+	theme: {
+		extend: {
+			colors: {
+				'custom-blue': '#179afc',
+			}
+		},
+	},
+	plugins: [],
+}
