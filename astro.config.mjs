5d824e27ab28e7e9c3226fe05851dd056c2d0da0
Adrián Pisabarro García <adrian.pisabarro.garcia@gmail.com>
2024-04-30 22:12:18 +0200

    Add analytics of vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,7 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  })
 });
